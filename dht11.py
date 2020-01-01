928c9e58e6d1f67916f98f39a52fa9cdec1d144b
Your Name <you@example.com>
2020-01-01 14:45:38 +0100

    update

--- dht11.py
+++ dht11.py
@@ -6,14 +6,14 @@ import Adafruit_DHT
 
 temp_id = 1
 hum_id = 2
-username = os.environ['WEATHERAPI_USERNAME']
-password = os.environ['WEATHERAPI_PASSWORD']
+username = os.environ['AUTH_USERNAME']
+password = os.environ['AUTH_PASSWORD']
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print 'Temp: {0:0.1f}C,  Humidity: {1:0.1f}%'.format(temperature, humidity)
-    print "curl -X POST -d 'value={0:0d}' -u {1}:{2} https://weather.scapp.io/sensor/{3}/value".format(int(temperature), username, password, temp_id)
-    os.system("curl -X POST -d 'value={0:0d}' -u {1}:{2} https://weather.scapp.io/sensor/{3}/value".format(int(temperature), username, password, temp_id))
-    print "curl -X POST -d 'value={0:0d}' -u {1}:{2} https://weather.scapp.io/sensor/{3}/value".format(int(humidity), username, password, hum_id)
-    os.system("curl -X POST -d 'value={0:0d}' -u {1}:{2} https://weather.scapp.io/sensor/{3}/value".format(int(humidity), username, password, hum_id))
-    time.sleep(120)
+    print "curl -X POST -d 'value={0:0d}' -u {1}:{2} https://home-info.scapp.io/sensor/{3}/value".format(int(temperature), username, password, temp_id)
+    os.system("curl -X POST -d 'value={0:0d}' -u {1}:{2} https://home-info.scapp.io/sensor/{3}/value".format(int(temperature), username, password, temp_id))
+    print "curl -X POST -d 'value={0:0d}' -u {1}:{2} https://home-info.scapp.io/sensor/{3}/value".format(int(humidity), username, password, hum_id)
+    os.system("curl -X POST -d 'value={0:0d}' -u {1}:{2} https://home-info.scapp.io/sensor/{3}/value".format(int(humidity), username, password, hum_id))
+    time.sleep(300)
