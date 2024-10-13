23456f08ee5823cec0db14074d2b3f4d7beef82b
JamesClonk <jamesclonk@jamesclonk.ch>
2024-10-13 20:34:29 +0200

    adjust goble

--- report_co2_sensor.sh
+++ report_co2_sensor.sh
@@ -7,7 +7,7 @@ CO2_SENSOR_ID=22
 while true; do
   export GOBLE_OUTPUT=$(goble EE:C8:36:2C:CB:39 2>/dev/null)
   export TEMP=$(echo "${GOBLE_OUTPUT}" | grep 'Temperature' | awk '{print int($2)-1;}')
-  export HUM=$(echo "${GOBLE_OUTPUT}" | grep 'Humidity' | awk '{print int($2)+5;}')
+  export HUM=$(echo "${GOBLE_OUTPUT}" | grep 'Humidity' | awk '{print int($2)+6;}')
   export CO2_PPM=$(echo "${GOBLE_OUTPUT}" | grep 'CO2' | awk '{print $3;}')
   echo "Temperature: ${TEMP}°C"
   echo "Humidity: ${HUM}% RH"
@@ -23,6 +23,6 @@ while true; do
     curl -X POST -d "value=${CO2_PPM}" -u "${AUTH_USERNAME}:${AUTH_PASSWORD}" "https://home-info.jamesclonk.io/sensor/${CO2_SENSOR_ID}/value"
   fi
 
-  sleep 555
+  sleep 666
 done
 
