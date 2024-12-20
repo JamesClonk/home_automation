d291c724ad987f933e403ef1aa960fae80093c4b
JamesClonk <jamesclonk@jamesclonk.ch>
2024-12-20 22:01:11 +0100

    washroom

--- report_shelly_washroom_trv.sh
+++ report_shelly_washroom_trv.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 SHELLY_DEVICE_ID=34cdb078bc64
-SENSOR_POS_ID=32
-SENSOR_BAT_ID=42
+SENSOR_POS_ID=41
+SENSOR_BAT_ID=44
 
 while true; do
   export SHELLY_DATA=$(curl -s ${SHELLY_CLOUD_URL}/device/status -d "id=${SHELLY_DEVICE_ID}&auth_key=${SHELLY_AUTH_KEY}")
-  export SHELLY_EPOCH=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:200".status.sys.unixtime')
-  export SHELLY_VALVE_POS=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:200".status."trv:0".pos')
-  export SHELLY_BATTERY_CHARGE=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv:200".battery')
+  export SHELLY_EPOCH=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:201".status.sys.unixtime')
+  export SHELLY_VALVE_POS=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:201".status."trv:0".pos')
+  export SHELLY_TARGET_TEMP=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:201".status."trv:0".target_C')
+  export SHELLY_BATTERY_CHARGE=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv:201".battery')
 
-  #echo ${SHELLY_DATA} | jq -r '.data.device_status'
+  #echo "${SHELLY_DATA}" | jq -r '.data.device_status'
   echo "Epoch: ${SHELLY_EPOCH}"
   echo "Valve position: ${SHELLY_VALVE_POS}%"
+  echo "Target temperature: ${SHELLY_TARGET_TEMP}°C"
   echo "Battery charge: ${SHELLY_BATTERY_CHARGE}%"
 
   export EPOCH_CUTOFF=$(date --date "20 minutes ago" +'%s')
