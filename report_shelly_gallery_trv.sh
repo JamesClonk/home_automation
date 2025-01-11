b97b6e2fafa87c72529fa571899be42f7ee4c22d
JamesClonk <jamesclonk@jamesclonk.ch>
2025-01-11 11:45:36 +0100

    enable gallery trv 2

--- report_shelly_gallery_trv.sh
+++ report_shelly_gallery_trv.sh
@@ -8,6 +8,7 @@ SENSOR_BAT_ID_TWO=48
 
 while true; do
   export SHELLY_DATA=$(curl -s ${SHELLY_CLOUD_URL}/device/status -d "id=${SHELLY_DEVICE_ID}&auth_key=${SHELLY_AUTH_KEY}")
+  #echo ${SHELLY_DATA} | jq -r '.data.device_status'
 
   echo "gallery main ..."
   export SHELLY_EPOCH=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:202".status.sys.unixtime')
@@ -29,23 +30,23 @@ while true; do
 
   sleep 7
 
-  # echo "gallery rig ..."
-  # export SHELLY_EPOCH=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:207".status.sys.unixtime')
-  # export SHELLY_VALVE_POS=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:207".status."trv:0".pos')
-  # export SHELLY_TARGET_TEMP=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:207".status."trv:0".target_C')
-  # export SHELLY_BATTERY_CHARGE=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv:207".battery')
+  echo "gallery rig ..."
+  export SHELLY_EPOCH=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:203".status.sys.unixtime')
+  export SHELLY_VALVE_POS=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:203".status."trv:0".pos')
+  export SHELLY_TARGET_TEMP=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv_rstatus:203".status."trv:0".target_C')
+  export SHELLY_BATTERY_CHARGE=$(echo ${SHELLY_DATA} | jq -r '.data.device_status."blutrv:203".battery')
 
-  # echo "Epoch: ${SHELLY_EPOCH}"
-  # echo "Valve position: ${SHELLY_VALVE_POS}%"
-  # echo "Target temperature: ${SHELLY_TARGET_TEMP}°C"
-  # echo "Battery charge: ${SHELLY_BATTERY_CHARGE}%"
+  echo "Epoch: ${SHELLY_EPOCH}"
+  echo "Valve position: ${SHELLY_VALVE_POS}%"
+  echo "Target temperature: ${SHELLY_TARGET_TEMP}°C"
+  echo "Battery charge: ${SHELLY_BATTERY_CHARGE}%"
 
-  # export EPOCH_CUTOFF=$(date --date "20 minutes ago" +'%s')
-  # echo "Epoch cutoff: ${EPOCH_CUTOFF}"
-  # if [[ ${SHELLY_EPOCH} > ${EPOCH_CUTOFF} ]]; then
-  #   curl -X POST -d "value=${SHELLY_VALVE_POS}" -u "${AUTH_USERNAME}:${AUTH_PASSWORD}" "https://home-info.jamesclonk.io/sensor/${SENSOR_POS_ID_TWO}/value"
-  #   curl -X POST -d "value=${SHELLY_BATTERY_CHARGE}" -u "${AUTH_USERNAME}:${AUTH_PASSWORD}" "https://home-info.jamesclonk.io/sensor/${SENSOR_BAT_ID_TWO}/value"
-  # fi
+  export EPOCH_CUTOFF=$(date --date "20 minutes ago" +'%s')
+  echo "Epoch cutoff: ${EPOCH_CUTOFF}"
+  if [[ ${SHELLY_EPOCH} > ${EPOCH_CUTOFF} ]]; then
+    curl -X POST -d "value=${SHELLY_VALVE_POS}" -u "${AUTH_USERNAME}:${AUTH_PASSWORD}" "https://home-info.jamesclonk.io/sensor/${SENSOR_POS_ID_TWO}/value"
+    curl -X POST -d "value=${SHELLY_BATTERY_CHARGE}" -u "${AUTH_USERNAME}:${AUTH_PASSWORD}" "https://home-info.jamesclonk.io/sensor/${SENSOR_BAT_ID_TWO}/value"
+  fi
 
   sleep 777
 done
